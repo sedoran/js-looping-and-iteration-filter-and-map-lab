a876a0baa2ad78e66f25b1d8e77746faa84c357a
Steven Doran <steven.doran@flatironschool.com>
2019-12-09 17:13:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, amount) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > amount
+  })
+}
+
+function driverNamesWithRevenueOver(drivers, amount) {
+  return driversWithRevenueOver(drivers, amount)
+  .map(driver => driver.name)
+}
+
+
+function exactMatch(drivers, details) {
+  return drivers.filter(function (driver) {
+    return driver.name === details.name || driver.revenue === details.revenue
+  })
+}
+
+function exactMatchToList(drivers, details) {
+  return exactMatch(drivers, details)
+  .map(driver => driver.name)
+}
+
+
